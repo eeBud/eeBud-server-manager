27258f023bd6c71e8ca226220ff57f2caadd3356
eeBud <114733448+eeBud@users.noreply.github.com>
2022-11-05 14:03:10 +0100

    Add files via upload

---
+++ examples/PAJ7620/PAJ7620.ino
@@ -0,0 +1,49 @@
+#include <eeBudServerManager.h>
+#include <DFRobot_PAJ7620U2.h>
+
+eeBudServerManager Serveur("eeBud - Contrôleur gestuel");
+
+int Bouton_Veille;
+int Info_Gestuel;
+String URL_Gestuel;
+
+DFRobot_PAJ7620U2 paj;
+
+void setup() {
+
+  Serveur.Init(); 
+  Serveur.AddParamText("URL Gestuel", "");
+  Info_Gestuel = Serveur.IndexAddInfo("Geste", "", "50");  
+  URL_Gestuel = Serveur.GetParamText("URL Gestuel");
+
+  while(paj.begin() != 0){    
+    delay(500);
+  }
+  paj.setGestureHighRate(true);
+
+  pinMode(LED, OUTPUT);
+  blynk(4, 100);
+}
+
+
+void loop() {
+  Serveur.IndexGetEvent();
+
+  DFRobot_PAJ7620U2::eGesture_t Geste = paj.getGesture();
+  if (Geste != paj.eGestureNone) {
+
+    String S_Geste = paj.gestureDescription(Geste);
+    
+    if (URL_Gestuel != "") {
+      String Temp = URL_Gestuel;
+      Temp.replace("#VALUE#", String(S_Geste));
+      Serveur.HTTPPost(Temp);
+      delay(500);
+    }
+
+    Serveur.IndexSetInfo(Info_Gestuel, S_Geste);    
+    
+  }
+  
+}
+
